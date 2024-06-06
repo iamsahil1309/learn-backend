2bba78c3a989b2d602a220c47d2bf866818da6c8
Sahil Singh <kswayam2000@gmail.com>
2024-06-06 22:12:46 +0530

    f

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload file to cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("file is uploaded on cloudinary.", response.url);
+    return response;
+
+  } catch (error) {
+        fs.unlinkSync(localFilePath) //remove locally sav temprory file as upload operation gets failed
+        return null
+  }
+};
+
+
+export  { uploadOnCloudinary }
